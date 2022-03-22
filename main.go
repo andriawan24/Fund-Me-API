64a536ea2ac43c574590d4f47f0da53e5d2fabbc
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2022-03-22 19:47:46 +0700

    update cors

--- main.go
+++ main.go
@@ -80,7 +80,8 @@ func main() {
 
 func corsMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://0.0.0.0:3000")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://103.181.143.204:3000")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://92.168.0.101:3000")
 		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
