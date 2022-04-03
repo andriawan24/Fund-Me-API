efb70390048972a9cc95ffb4601e1cd24a389e8a
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2022-04-03 16:00:59 +0700

    Add layouting for CMS

--- main.go
+++ main.go
@@ -81,6 +81,7 @@ func main() {
 func corsMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://103.181.143.204:3000")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:3000")
 		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
